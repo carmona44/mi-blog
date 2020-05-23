cf1227f5f585302898006b14f950040d93fab12f
Daniel <dcacarmona44@gmail.com>
2020-05-23 18:39:15 +0200

    Estilos principales de las cards

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
+  <!--<div>
     <p v-html="post.date" />
     <h1 v-html="post.title" />
     <p v-html="post.description" />
     <b> {{post.timeToRead}} min read </b>
     <g-link :to="post.path">Read More...</g-link>
+  </div>-->
+  <div class="card-style rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="../img/prueba.png" alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-center" v-html="post.title"></div>
+      <div class="text-center text-sm text-gray-600">
+        <span v-html="post.date" />
+        <b> {{post.timeToRead}} min read </b>
+      </div>      
+      <p class="text-gray-700 text-base" v-html="post.description" />
+      <g-link :to="post.path">Read More...</g-link>
+    </div>
+    <div class="px-6 py-4">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    </div>
   </div>
 </template>
 
@@ -13,3 +30,17 @@ export default {
   props: ["post"],
 };
 </script>
+
+<style scope>
+.card-style {
+  max-width: 65%;
+  margin: 1.5rem auto;
+}
+
+@media screen and (max-width: 600px) {
+  .card-style {
+    max-width: 90%;
+    margin: 1.5rem auto;
+  }
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,9 +4,9 @@
             <div class="mb-4">
                 <img class="w-24 mx-auto rounded-full object-cover object-center" src="https://i1.pngguru.com/preview/137/834/449/cartoon-cartoon-character-avatar-drawing-film-ecommerce-facial-expression-png-clipart.jpg" alt="Avatar Upload" />
             </div>
-            <div class="text-sm">
+            <div>
                 <p class="text-gray-900 leading-none">Daniel Carmona | Blog</p>
-                <p class="text-gray-600">Bienvenido a mi blog</p>
+                <p class="text-sm text-gray-600">Bienvenido a mi blog</p>
             </div>
         </div>
     </div>
