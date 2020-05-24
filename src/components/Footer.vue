cf82a98e9403be6cf08880b149f5288a4e88a0c2
Daniel <dcacarmona44@gmail.com>
2020-05-24 13:58:26 +0200

    Se añade descripción e iconos de RRSS del perfil

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class='w-full text-xs text-center p-4 mt-16 mb-10'>
-      <p>Made on Earth by human. Built with Gridsome, Tailwind and a lot of ❤️</p>
+      <p>Made on Earth by human. Built with Gridsome, Tailwind, iconmonstr and a lot of ❤️</p>
       <p>Copyright &copy; {{ new Date().getFullYear()}} | 
         <a href="https://carmona44.github.io/portfolio/" class="underline hover:font-bold">Daniel Carmona Alarcón</a>
       </p>
