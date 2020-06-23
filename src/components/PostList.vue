9386bcf4beba2615c98bcf0cb34db55b3b4cfbc6
Daniel <dcacarmona44@gmail.com>
2020-06-23 19:42:55 +0200

    Fixed nested g-link bug

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,23 +1,23 @@
 <template>
-  <g-link :to="post.path">
     <div class="card-style rounded overflow-hidden shadow-lg">
-      <g-image v-if="post.img" class="w-full" :src="post.img" alt="Sunset in the mountains" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl text-center" v-html="post.title"></div>
-        <div class="text-center text-xs text-gray-600">
-          <span v-html="post.date" />
-          <b> {{post.timeToRead}} min lectura </b>
-        </div>      
-        <p class="text-gray-800 text-base pt-3 pb-3" v-html="post.description" />
-        <p :to="post.path">Leer más...</p>
-      </div>
+      <g-link :to="post.path">
+        <g-image v-if="post.img" class="w-full" :src="post.img" alt="Sunset in the mountains" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl text-center" v-html="post.title"></div>
+          <div class="text-center text-xs text-gray-600">
+            <span v-html="post.date" />
+            <b> {{post.timeToRead}} min lectura </b>
+          </div>      
+          <p class="text-gray-800 text-base pt-3 pb-3" v-html="post.description" />
+          <p :to="post.path">Leer más...</p>
+        </div>
+      </g-link>
       <div class="px-6 py-4">
         <g-link class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2" v-for="tag in post.tags" :key="tag.id" :to="tag.path">
           <span>#</span> {{ tag.title }}
         </g-link>
       </div>
     </div>
-  </g-link>
 </template>
 
 <script>
