5c0421f728b26fbf691d0bb461c47115bc26bd48
Daniel <dcacarmona44@gmail.com>
2020-06-23 18:37:24 +0200

    Added tailwind config with purge

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+
     siteName: 'Daniel Carmona | Blog',
     siteDescription: 'Bienvenido al blog de Daniel Carmona. Ciclismo, programación y muchas otras historias.',
 
@@ -13,8 +14,12 @@ module.exports = {
         Tag: '/tag/:id'
     },
 
-    plugins: [
-        { use: 'gridsome-plugin-tailwindcss' },
+    plugins: [{
+            use: 'gridsome-plugin-tailwindcss',
+            options: {
+                config: './tailwind.config.js'
+            }
+        },
         {
             use: '@gridsome/source-filesystem',
             options: {
